012a3441fd8c6ae17c9f9bf2e78c09d0a7ded8b2
Anna Yurevych <annayurevych@gmail.com>
2024-09-18 11:29:19 +0300

    Add project files.

---
+++ LibraryDomain/Model/Author.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LibraryDomain;
+
+public partial class Author
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Info { get; set; }
+
+    public virtual ICollection<Book> Books { get; set; } = new List<Book>();
+}
---
+++ LibraryDomain/Model/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryDomain;
+
+public partial class Book
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    [Display(Name = "Author")]
+    public int AuthorId { get; set; }
+
+    [Display(Name = "Category")]
+    public int CategoryId { get; set; }
+
+    [Display(Name="Cover")]
+    public string? FileLink { get; set; }
+
+    public virtual Author Author { get; set; } = null!;
+
+    public virtual Category Category { get; set; } = null!;
+}
